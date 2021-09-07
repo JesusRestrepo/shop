36cfcfba8ed2247973fb635896e63907ada64fa4
JesusRestrepo <jesusrestrepo2003@gmail.com>
2021-09-07 13:54:02 -0500

    V1

--- src/components/Moto.js
+++ src/components/Moto.js
@@ -7,27 +7,27 @@ const Moto = () => {
                 <div className="row">
                     <div className="col">
                         <div className="card mt-3">
-                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwCbT7bf5nRu0w2NpOzYfb6qt-7aeyzbcRIJoukLlLjiBUfZAuJdGKNYMzfqCoWyC4jhKg72SF&usqp=CAc" className="imagen" alt="..."/>
+                            <img src="https://www.pontgrup.com/blog/wp-content/uploads/2018/03/gadgets-moto.jpg" className="imagen" alt="..."/>
                             <div className="card-body">
-                                <h5 className="card-title">Cargador Inalambrico para carro</h5>
+                                <h5 className="card-title">Soporte para celular moto</h5>
                                 <a className="btn btn-primary">Go somewhere</a>
                             </div>
                         </div>
                     </div>
                     <div className="col">
                         <div className="card mt-3">
-                                <img src="https://i.blogs.es/078455/vic/450_1000.jpg" className="imagen" alt="..."/>
+                                <img src="https://www.pontgrup.com/blog/wp-content/uploads/2018/03/sena30k.jpg" className="imagen" alt="..."/>
                                 <div className="card-body">
-                                    <h5 className="card-title">Conector USB para celular y música</h5>
+                                    <h5 className="card-title">Intercomunicador</h5>
                                     <a className="btn btn-primary">Go somewhere</a>
                                 </div>
                         </div>
                     </div>
                     <div className="col">
                         <div className="card mt-3">
-                                <img src="https://static3.elcorreo.com/www/multimedia/202011/04/media/cortadas/615GbEDqwQL._AC_SL1500_-kVzH-U120669777876mWF-624x385@El%20Correo.jpg" className="imagen" alt="..."/>
+                                <img src="https://koneet.co/wp-content/uploads/2020/10/785917-MCO43546416789_092020.jpg" className="imagen" alt="..."/>
                                 <div className="card-body">
-                                    <h5 className="card-title">Receptor bluetooth</h5>
+                                    <h5 className="card-title">Seguro de freno para motos</h5>
                                     <a className="btn btn-primary">Go somewhere</a>
                                 </div>
                         </div>
@@ -37,27 +37,27 @@ const Moto = () => {
                 <div className="row">
                 <div className="col">
                     <div className="card mt-3">
-                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwCbT7bf5nRu0w2NpOzYfb6qt-7aeyzbcRIJoukLlLjiBUfZAuJdGKNYMzfqCoWyC4jhKg72SF&usqp=CAc" className="imagen" alt="..."/>
+                        <img src="https://motostorebogota.com/wp-content/uploads/2019/07/C_1_4.jpg" className="imagen" alt="..."/>
                         <div className="card-body">
-                            <h5 className="card-title">Cargador Inalambrico para carro</h5>
+                            <h5 className="card-title">Puerto USB para motos</h5>
                             <a className="btn btn-primary">Go somewhere</a>
                         </div>
                     </div>
                 </div>
                 <div className="col">
                     <div className="card mt-3">
-                            <img src="https://i.blogs.es/078455/vic/450_1000.jpg" className="imagen" alt="..."/>
+                            <img src="https://lahora.com.ec/contenido/cache/49/gadget-un-casco-inteligente-para-moto-imagen-1-_2018082062827-2000x2000.jpg" className="imagen" alt="..."/>
                             <div className="card-body">
-                                <h5 className="card-title">Conector USB para celular y música</h5>
+                                <h5 className="card-title">Casco inteligente</h5>
                                 <a className="btn btn-primary">Go somewhere</a>
                             </div>
                     </div>
                 </div>
                 <div className="col">
                     <div className="card mt-3">
-                            <img src="https://static3.elcorreo.com/www/multimedia/202011/04/media/cortadas/615GbEDqwQL._AC_SL1500_-kVzH-U120669777876mWF-624x385@El%20Correo.jpg" className="imagen" alt="..."/>
+                            <img src="https://www.dhresource.com/0x0/f2/albu/g4/M01/BB/19/rBVaEFmjdwSAdlB1AAGnccuMkO0893.jpg/motorcycle-switches-on-off-button-headlight.jpg" className="imagen" alt="..."/>
                             <div className="card-body">
-                                <h5 className="card-title">Receptor bluetooth</h5>
+                                <h5 className="card-title">Interruptor para motocicletas</h5>
                                 <a className="btn btn-primary">Go somewhere</a>
                             </div>
                     </div>
@@ -67,18 +67,18 @@ const Moto = () => {
             <div className="row">
                 <div className="col">
                     <div className="card mt-3">
-                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwCbT7bf5nRu0w2NpOzYfb6qt-7aeyzbcRIJoukLlLjiBUfZAuJdGKNYMzfqCoWyC4jhKg72SF&usqp=CAc" className="imagen" alt="..."/>
+                        <img src="https://m.media-amazon.com/images/I/31VYhaX-bpS._SL500_.jpg" className="imagen" alt="..."/>
                         <div className="card-body">
-                            <h5 className="card-title">Cargador Inalambrico para carro</h5>
+                            <h5 className="card-title">Tablero digital moto</h5>
                             <a className="btn btn-primary">Go somewhere</a>
                         </div>
                     </div>
                 </div>
                 <div className="col">
                     <div className="card mt-3">
-                            <img src="https://i.blogs.es/078455/vic/450_1000.jpg" className="imagen" alt="..."/>
+                            <img src="https://apollo-virginia.akamaized.net/v1/files/ci1o5swk0ei12-CO/image" className="imagen" alt="..."/>
                             <div className="card-body">
-                                <h5 className="card-title">Conector USB para celular y música</h5>
+                                <h5 className="card-title">GPS para motos</h5>
                                 <a className="btn btn-primary">Go somewhere</a>
                             </div>
                     </div>
