24796ef28616582b7b188f78e04c868a93e5888b
JesusRestrepo <jesusrestrepo2003@gmail.com>
2021-09-15 14:03:10 -0500

    V1

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,26 +1,51 @@
 import React from "react";
-import { Button } from "reactstrap";
 
 import './home.css';
-import FullWidthTabs from './Products';
-import rr from './rr.gif';
-
 
 const Home = () => {
     return(
         <React.Fragment>
             <div className="espacio"></div>
-            <div className="contenedor-principal">
+            <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
+                <div className="carousel-inner">
+                    <div className="carousel-item active">
+                        <img src="https://miro.medium.com/max/1024/1*vxjAHkrXbGG6gOiPZgjeZA.jpeg" 
+                        className="d-block w-100" alt="..." />
+                    </div>
+                    <div className="carousel-item">
+                        <img src="https://conceptodefinicion.de/wp-content/uploads/2021/01/tecnologia-.jpg" 
+                        className="d-block w-100" width="100%" alt="..." />
+                    </div>
+                    <div className="carousel-item">
+                        <img src="https://cdn-3.expansion.mx/dims4/default/7d3824b/2147483647/strip/true/crop/1000x667+0+0/resize/800x534!/format/webp/quality/90/?url=https%3A%2F%2Fcdn-3.expansion.mx%2Ff0%2F1d%2Fd0b7fd994e67a9f48564db5f7507%2Fistock-856757428-copy.jpg" 
+                        className="d-block w-100" width="100%" alt="..." />
+                    </div>
+                </div>
+                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Previous</span>
+                </button>
+                <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Next</span>
+                </button>
+            </div>
+
+            <div className="Buttons">
                 <div className="row">
                     <div className="col">
-                        <div className="content-1">
-                            <img src={rr} className="img"/>   
-                        </div>                            
+                        <div className="carro">
+                            carros
+                        </div>  
                     </div>
                     <div className="col">
-                        <div className="content-1">
-                            <h1>BIENVENIDO</h1>
-                            <Button color="primary" href="/shop/products">click me</Button>
+                        <div className="moto">
+                            motos
+                        </div>
+                    </div>    
+                    <div className="col">
+                        <div className="otro">
+                            otros
                         </div>
                     </div>
                 </div>
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -6,18 +6,18 @@ const Navbar = () => {
             <div className="container-fluid">
                 <a className="navbar-brand" href="/shop/home">House Technology</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
+                    <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
                         <li className="nav-item">
-                        <a className="nav-link" aria-current="page" href="/shop/home">Home</a>
+                            <a className="nav-link" aria-current="page" href="/shop/home">Home</a>
                         </li>
                         <li className="nav-item">
-                        <a className="nav-link" href="/shop/nosotros">Nosotros</a>
+                            <a className="nav-link" href="/shop/nosotros">Nosotros</a>
                         </li>
                         <li className="nav-item">
-                        <a className="nav-link" href="/shop/products">Productos</a>
+                            <a className="nav-link" href="/shop/products">Productos</a>
                         </li>
                     </ul>
                 </div>
